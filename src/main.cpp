ae10af45a1b13ccc4ca4e4981ff14d6fc2996fa0
Tyler <lorenarcdev@gmail.com>
2024-05-01 18:22:44 -0400

    Implemented custom logger from header only library.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
 #include <raylib.h>
+#include <plog/Log.h>
+#include <plog/Init.h>
+#include <plog/Formatters/TxtFormatter.h>
+#include <plog/Appenders/RollingFileAppender.h>
+#include <plog/Appenders/ColorConsoleAppender.h>
 #include "defaults.h"
 
+using plog::RollingFileAppender, plog::TxtFormatter, 
+plog::ColorConsoleAppender;
 
-void resizeWindow(Rectangle &canvas) {
+
+/* For correcting the dimensions of the canvas to the correct window
+ * resolution. Sometimes the window my able change height just to 
+ * fit perfectly with the canvas' aspect ratio.*/
+void correctWindow(Rectangle &canvas) {
   int screen_width = GetScreenWidth();
   int screen_height = GetScreenHeight();
 
@@ -24,25 +35,40 @@ void resizeWindow(Rectangle &canvas) {
 }
 
 
+void setupCustomLogger() {
+  static RollingFileAppender<TxtFormatter> file_appender("logs/log.txt",
+                                                           1000000, 10);
+    static ColorConsoleAppender<TxtFormatter> console_appender;
+    plog::init(plog::verbose, &file_appender)
+      .addAppender(&console_appender);
+}
+
+
 int main() {
+  setupCustomLogger();
+  PLOGV << "Logger initialized.";
+  PLOGI << "Initizing game, and setting everything up...";
+
   SetConfigFlags(FLAG_WINDOW_RESIZABLE);
   InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, 
              "True Human Tribulation: Skirmish");
   SetTargetFPS(TARGET_FRAMERATE);
 
+  PLOGI << "Setting up canvas...";
   RenderTexture canvas = LoadRenderTexture(CANVAS_WIDTH, CANVAS_HEIGHT);
   Rectangle source = {0, 0, CANVAS_WIDTH, -CANVAS_HEIGHT};
   Rectangle dest = {0, 0, WINDOW_WIDTH, WINDOW_HEIGHT};
 
   Texture test_texture = LoadTexture("res_test.png");
 
+  PLOGV << "Everything seems good to go!";
   while (WindowShouldClose() == false) {
     if (IsWindowResized()) {
-      resizeWindow(dest);
+      correctWindow(dest);
     }
     if (IsKeyPressed(KEY_F11)) {
       ToggleBorderlessWindowed();
-      resizeWindow(dest);
+      correctWindow(dest);
     }
 
     BeginTextureMode(canvas);
@@ -62,5 +88,6 @@ int main() {
 
   UnloadRenderTexture(canvas);
   UnloadTexture(test_texture);
+  PLOGV << "Thanks for playing!";
   return 0;
 }
