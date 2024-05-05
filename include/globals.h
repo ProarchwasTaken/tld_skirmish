7fd485db6599bae209265113d075ccfd6cd54962
Tyler <lorenarcdev@gmail.com>
2024-05-05 13:15:34 -0400

    Defined the PLATFORM macro that will be different depending on the operating system running the game.

--- include/globals.h
+++ include/globals.h
@@ -2,6 +2,14 @@
 #pragma once
 #include <raylib.h>
 
+#if defined(_WIN32)
+  #define PLATFORM "windows"
+#elif defined(__linux__)
+  #define PLATFORM "linux"
+#elif defined(__unix__)
+  #define PLATFORM "linux"
+#endif
+
 namespace COLORS {
   extern Color *PALETTE;
 }
