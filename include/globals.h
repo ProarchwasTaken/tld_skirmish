c6acfb69f00590038ba6fdf7c67ecaf65bdd6c54
Tyler <lorenarcdev@gmail.com>
2024-05-07 13:30:57 -0400

    Written docstring for globals.h

--- include/globals.h
+++ include/globals.h
@@ -1,4 +1,8 @@
-// globals
+// globals.h
+/* This file contains a number of macros and external variables that all
+ * have the capacity to be different depending on the circumstances. Like
+ * what command line arguments are used when the game is ran, or what
+ * platform the game is running on.*/
 #pragma once
 #include <raylib.h>
 
