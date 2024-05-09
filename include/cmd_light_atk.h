2526f802acd7f08dfefb43f2141b272e0040b454
Tyler <lorenarcdev@gmail.com>
2024-05-09 11:20:32 -0400

    Declared and defined the basics of the Light Attack command.

---
+++ include/cmd_light_atk.h
@@ -0,0 +1,10 @@
+// cmd_light_atk.h
+#pragma once
+#include "base/combatant.h"
+#include "base/action_command.h"
+
+
+class LightAttack : public ActionCommand {
+public:
+  LightAttack(Combatant &user);
+};
---
+++ src/cmd_light_atk.cpp
@@ -0,0 +1,10 @@
+// cmd_light_attack.cpp
+#include "base/combatant.h"
+#include "base/action_command.h"
+#include "cmd_light_atk.h"
+
+LightAttack::LightAttack(Combatant &user) : 
+  ActionCommand(user, "Light Attack", 0.2, 0.2, 0.5)
+{
+
+}
