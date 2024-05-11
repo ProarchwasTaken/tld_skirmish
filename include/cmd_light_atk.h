7217c337a5f714abfa9a0d33d3fdbcec0c54913e
Tyler <lorenarcdev@gmail.com>
2024-05-11 12:01:32 -0400

    Defined a hurtbox that will be setup on initialization. The hurtbox will be used to check for collisions during the actSequence.

--- include/cmd_light_atk.h
+++ include/cmd_light_atk.h
@@ -1,5 +1,6 @@
 // cmd_light_atk.h
 #pragma once
+#include <raylib.h>
 #include "base/combatant.h"
 #include "base/action_command.h"
 
@@ -8,6 +9,10 @@ class LightAttack : public ActionCommand {
 public:
   LightAttack(Combatant &user);
 
+  void setupHurtbox();
+
   void draw();
   void drawDebug();
+private:
+  Rectangle hurtbox;
 };
--- src/cmd_light_atk.cpp
+++ src/cmd_light_atk.cpp
@@ -1,4 +1,5 @@
 // cmd_light_attack.cpp
+#include <raylib.h>
 #include "base/combatant.h"
 #include "base/action_command.h"
 #include "cmd_light_atk.h"
@@ -6,11 +7,27 @@
 LightAttack::LightAttack(Combatant &user) : 
   ActionCommand(user, "Light Attack", 0.1, 0.1, 0.2)
 {
+  setupHurtbox();
+}
 
+void LightAttack::setupHurtbox() {
+  float width = 48;
+  float half_width = width / 2;
+
+  float height = 16;
+
+  float x_offset = half_width * user->direction;
+
+  float x = (user->position.x - half_width) + x_offset;
+  float y = user->position.y - 56;
+
+  hurtbox = {x, y, width, height};
 }
 
 void LightAttack::draw() {
-
+  if (user->state == ACT) {
+    DrawRectangleRec(hurtbox, MAROON);
+  } 
 }
 
 void LightAttack::drawDebug() {
