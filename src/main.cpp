c35295c56fce1742b64917fc4c40f05ba81594b4
Tyler <lorenarcdev@gmail.com>
2024-06-03 12:13:57 -0400

    parseArguement now returns a integer resembling the starting scene. THis could be changed through the use of command line arguments. As a result of this, the debug scene is no longer tied to debug mode.

--- src/main.cpp
+++ src/main.cpp
@@ -19,35 +19,18 @@ vector<Texture*> sprites::player;
 bool DEBUG_MODE = false;
 
 void setupCustomLogger();
-void parseArguments(int argc, char *argv[]);
+int parseArguments(int argc, char *argv[]);
 
 
 int main(int argc, char *argv[]) {
   setupCustomLogger();
-  if (PLATFORM == WINDOWS) {
-    PLOGV << "Operating System: Windows";
-  }
-  else if (PLATFORM == LINUX) {
-    PLOGV << "Operating System: Linux";
-  }
-
-  if (argc > 1) {
-    parseArguments(argc, argv);
-  }
+  int start_scene = parseArguments(argc, argv);
 
   PLOGV << "Initializing the game...";
   SetConfigFlags(FLAG_WINDOW_RESIZABLE);
   InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, 
              "True Human Tribulation: Skirmish");
   SetTargetFPS(TARGET_FRAMERATE);
-
-  int start_scene;
-  if (DEBUG_MODE) {
-    start_scene = SCENE_DEBUG;
-  }
-  else {
-    start_scene = SCENE_MENU;
-  }
 
   Game game(start_scene);
 
@@ -56,6 +39,7 @@ int main(int argc, char *argv[]) {
     if (IsWindowResized()) {
       game.correctWindow();
     }
+
     if (IsKeyPressed(KEY_F11)) {
       PLOGI << "Toggling borderless fullscreen.";
       ToggleBorderlessWindowed();
@@ -76,18 +60,31 @@ void setupCustomLogger() {
   plog::init(plog::verbose, &file_appender)
     .addAppender(&console_appender);
   PLOGV << "Logger initialized.";
+
+  if (PLATFORM == WINDOWS) {
+    PLOGV << "Operating System: Windows";
+  }
+  else if (PLATFORM == LINUX) {
+    PLOGV << "Operating System: Linux";
+  }
 }
 
 
-void parseArguments(int argc, char *argv[]) {
+int parseArguments(int argc, char *argv[]) {
   PLOGI << "Parsing command line arguments.";
+  int start_scene = SCENE_MENU;
 
   for (int x = 0; x < argc; x++) {
     string arg = argv[x];
-    if (arg == "--debug") {
+    if (arg == "-d") {
       PLOGV << "Starting the game in debug mode.";
       DEBUG_MODE = true;
     }
+    else if (arg == "--debug_scene") {
+      start_scene = SCENE_DEBUG;
+    }
   }
+
+  return start_scene;
 }
 
