3214e6e99700668e41db3bb136d400fd4fae5b5e
Tyler <lorenarcdev@gmail.com>
2024-06-03 13:33:00 -0400

    Revert "Defined a scene variable. Had to do a lot of refactoring."
    This reverts commit 1bab5192bd50b398bda18a5e6fbfb57b1836a0a3.

--- src/game.cpp
+++ src/game.cpp
@@ -3,19 +3,28 @@
 #include <raymath.h>
 #include <memory>
 #include "defaults.h"
-#include "globals.h"
 #include "game.h"
-#include "scene_debug.h"
+#include "globals.h"
+#include "char_player.h"
+#include "enemy_dummy.h"
 #include <plog/Log.h>
 
-using std::make_unique;
+using std::make_shared;
 
 
 Game::Game() {
   setupCanvas();
   defineColorPalette();
 
-  scene = make_unique<DebugScene>();
+  test_room = LoadTexture("concept_art/test_room2.png");
+
+  player = make_shared<PlayerCharacter>(enemies);
+  enemies.push_back(make_shared<DummyEnemy>(*player, (Vector2){64, 208}));
+
+  camera.target = {player->position.x, 120};
+  camera.offset = {CANVAS_WIDTH / 2.0, CANVAS_HEIGHT / 2.0};
+  camera.zoom = 1;
+  camera.rotation = 0;
 }
 
 Game::~Game() {
@@ -23,8 +32,14 @@ Game::~Game() {
   UnloadRenderTexture(canvas);
   UnloadImagePalette(COLORS::PALETTE);
 
-  scene.reset();
+  UnloadTexture(test_room);
+  player.reset();
 
+  PLOGI << "Deleting all leftover enemies from memory.";
+  for (auto enemy : enemies) {
+    enemy.reset();
+  }
+  enemies.clear();
   PLOGV << "Thanks for playing!";
 }
 
@@ -71,16 +86,76 @@ void Game::defineColorPalette() {
   UnloadImage(palette);
 }
 
+void Game::cameraFollowPlayer(double &delta_time) {
+  bool player_offCenter = camera.target.x != player->position.x;
+  if (player_offCenter == false) {
+    return;
+  }
+
+  float x_difference = player->position.x - camera.target.x;
+  float next_x = x_difference / 8;
+  int direction;
+
+  if (x_difference > 0) {
+    direction = 1;
+  }
+  else if (x_difference < 0) {
+    direction = -1;
+  }
+
+  float boundary = CAMERA_BOUNDS * direction;
+  float half_width = CANVAS_WIDTH / 2.0;
+  float screen_side = camera.target.x + (half_width * direction);
+
+  bool already_at_boundary = screen_side == boundary; 
+  if (already_at_boundary) {
+    return;
+  }
+
+  float offset = screen_side + next_x;
+
+  bool hit_left_bounds = offset <= -CAMERA_BOUNDS;
+  bool hit_right_bounds = offset >= CAMERA_BOUNDS;
+  if (hit_left_bounds) {
+    camera.target.x = -CAMERA_BOUNDS + half_width; 
+  }
+  else if (hit_right_bounds) {
+    camera.target.x = CAMERA_BOUNDS - half_width;
+  }
+  else {
+    camera.target.x += next_x * delta_time;
+    if (FloatEquals(camera.target.x, player->position.x)) {
+      camera.target.x = player->position.x;
+    }
+  }
+}
+
 void Game::refresh() {
   delta_time = GetFrameTime() * TARGET_FRAMERATE;
+  player->inputPressed();
+  player->inputReleased();
 
-  scene->checkInput();
-  scene->updateScene(delta_time);
+  player->update(delta_time);
+  cameraFollowPlayer(delta_time);
+
+  for (auto enemy : enemies) {
+    enemy->update(delta_time);
+  }
 
   BeginTextureMode(canvas);
   {
+    BeginMode2D(camera);
+
     ClearBackground(BLACK);
-    scene->drawScene();
+    DrawTexture(test_room, -512, 0, WHITE);
+
+    for (auto enemy : enemies) {
+      enemy->draw();
+    }
+
+    player->draw();
+
+    EndMode2D();
   }
   EndTextureMode();
 
@@ -90,7 +165,17 @@ void Game::refresh() {
                    {0, 0}, 0, WHITE);
 
     if (DEBUG_MODE) {
-      scene->drawDebugInfo();
+      DrawText(TextFormat("FPS: %i", GetFPS()), 16, 16, 32, PURPLE);
+      DrawText(TextFormat("Player Position: (%02.02f, %02.02f)",
+                          player->position.x, player->position.y), 
+               16, 48, 32, PURPLE);
+      DrawText(TextFormat("Player Health: %i", player->health), 
+               16, 80, 32, PURPLE);
+      DrawText(TextFormat("Player State: %i", player->state), 
+               16, 112, 32, PURPLE);
+      DrawText(TextFormat("Camera X Difference: %f", 
+                          player->position.x - camera.target.x), 
+               16, 144, 32, PURPLE);
     }
   }
   EndDrawing();
