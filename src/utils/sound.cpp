29cd719478ad47fd2f6f0563025d32f43c2482f2
Tyler <lorenarcdev@gmail.com>
2024-06-22 13:57:13 -0400

    Now gets a random float if the sound is supposed to have a random pitch.

--- src/utils/sound.cpp
+++ src/utils/sound.cpp
@@ -1,11 +1,14 @@
 // utils/sound.cpp
 #include <cstddef>
+#include <random>
 #include <cstdint>
 #include <raylib.h>
 #include "globals.h"
 #include "audio.h"
 #include "utils.h"
 #include <plog/Log.h>
+
+using std::uniform_real_distribution;
 
 
 void SoundUtils::play(std::string sound_name) {
@@ -18,7 +21,17 @@ void SoundUtils::play(std::string sound_name) {
 
   uint8_t sound_id = meta_data->id;
   Sound *sound = &meta_data->manager->sound_effects[sound_id];
+  float pitch = 1.0;
 
+  if (meta_data->random_pitch) {
+    float min_pitch = meta_data->min_pitch;
+    float max_pitch = meta_data->max_pitch;
+
+    uniform_real_distribution<float> range(min_pitch, max_pitch);
+    pitch = range(RNG::generator);
+  }
+
+  SetSoundPitch(*sound, pitch);
   PlaySound(*sound);
 }
 
