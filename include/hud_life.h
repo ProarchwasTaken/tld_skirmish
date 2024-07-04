0fcb798616fa4ae1f1cfaa2660b3fd927367b860
Tyler <lorenarcdev@gmail.com>
2024-07-04 17:40:33 -0400

    Managed to get the text part of the hud done. It's starting to look exactly as it did in the mockup.

--- include/hud_life.h
+++ include/hud_life.h
@@ -8,10 +8,12 @@ class LifeHud {
 public:
   LifeHud(PlayerCharacter *player);
 
+  void alignText(const char* health_text);
   void draw();
 
 private:
   PlayerCharacter *player;
   
   Vector2 hud_position;
+  Vector2 text_position;
 };
--- src/hud/hud_life.cpp
+++ src/hud/hud_life.cpp
@@ -10,9 +10,23 @@ LifeHud::LifeHud(PlayerCharacter *player) {
   this->player = player;
 
   hud_position = {16, 212};
+  text_position = {0, hud_position.y - 2};
   PLOGI << "Initialized LifeHud.";
+}
+
+void LifeHud::alignText(const char* health_text) {
+  int size = fonts::skirmish->baseSize;
+  int width = MeasureTextEx(*fonts::skirmish, health_text, size, -3).x;
+
+  text_position.x = (hud_position.x + 79) - width;
 }
 
 void LifeHud::draw() {
   DrawTextureV(*sprites::hud_life[0], hud_position, WHITE);
+
+  auto *txt_hp = TextFormat("%i/%i", player->health, player->max_health);
+  alignText(txt_hp);
+
+  int size = fonts::skirmish->baseSize;
+  DrawTextEx(*fonts::skirmish, txt_hp, text_position, size, -3, WHITE);
 }
