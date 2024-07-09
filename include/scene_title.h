d1f5273e587e4ddc70858ae4f687f2b88c71f2b0
Tyler <lorenarcdev@gmail.com>
2024-07-09 17:49:07 -0400

    Added some basic text to the title scene.

--- include/scene_title.h
+++ include/scene_title.h
@@ -1,6 +1,7 @@
 // scene_title.h
 #pragma once
 #include <raylib.h>
+#include <string>
 #include <functional>
 #include "base/scene.h"
 
@@ -8,5 +9,22 @@
 class TitleScene : public Scene {
 public:
   TitleScene(std::function<void(int)> load_scene);
+
+  void setupTitle();
+  void setupCopyright();
+  void setupEnter();
+
   void drawScene() override;
+private:
+  std::string txt_title;
+  int title_size;
+  Vector2 title_position;
+
+  std::string txt_copyright;
+  int cpr_size;
+  Vector2 cpr_position;
+
+  std::string txt_enter;
+  int enter_size;
+  Vector2 enter_position;
 };
--- src/scenes/scene_title.cpp
+++ src/scenes/scene_title.cpp
@@ -1,26 +1,67 @@
 // scenes/scene_title.cpp
 #include <raylib.h>
 #include <functional>
+#include <string>
 #include "defaults.h"
 #include "globals.h"
 #include "scene_title.h"
+#include <plog/Log.h>
 
-using std::function;
+using std::function, std::string;
 
 
 TitleScene::TitleScene(function<void(int)> load_scene) : Scene(load_scene) 
 {
-
+  PLOGV << "Loading Title scene.";
+  setupTitle();
+  setupCopyright();
+  setupEnter();
+  PLOGV << "Title scene has been successfully loaded.";
 }
 
+void TitleScene::setupTitle() {
+  txt_title = "True Human Tribulation II";
+
+  title_size = fonts::skirmish->baseSize * 2;
+  int title_width = MeasureTextEx(*fonts::skirmish, txt_title.c_str(), 
+                                  title_size, -3).x;
+
+  title_position.x = (CANVAS_WIDTH / 2.0f) - title_width / 2.0;
+  title_position.y = 32;
+}
+
+void TitleScene::setupCopyright() {
+  txt_copyright = "@2024 Tyler Dillard";
+
+  cpr_size = fonts::skirmish->baseSize;
+  int cpr_width = MeasureTextEx(*fonts::skirmish, txt_copyright.c_str(), 
+                                cpr_size, -3).x;
+
+  cpr_position.x = CANVAS_WIDTH - cpr_width;
+  cpr_position.y = 228;
+}
+
+void TitleScene::setupEnter() {
+  if (IsGamepadAvailable(0)) {
+    txt_enter = "PRESS START";
+  }
+  else {
+    txt_enter = "PRESS ENTER";
+  }
+
+  enter_size = fonts::skirmish->baseSize;
+  int enter_width = MeasureTextEx(*fonts::skirmish, txt_enter.c_str(), 
+                                  enter_size, 0).x;
+
+  enter_position.x = (CANVAS_WIDTH / 2.0f) - enter_width / 2.0;
+  enter_position.y = 160;
+}
 
 void TitleScene::drawScene() {
-  int size = fonts::skirmish->baseSize * 2;
-  int width = MeasureTextEx(*fonts::skirmish, "True Human Tribulation II", 
-                            size, -3).x;
-  int half_width = width / 2;
-  Vector2 position = {(CANVAS_WIDTH / 2.0f) - half_width, 32};
-  DrawTextEx(*fonts::skirmish, "True Human Tribulation II", 
-             position, size, -3, COLORS::PALETTE[42]);
-
+  DrawTextEx(*fonts::skirmish, txt_title.c_str(), title_position, 
+             title_size, -3, COLORS::PALETTE[42]);
+  DrawTextEx(*fonts::skirmish, txt_enter.c_str(), enter_position, 
+             enter_size, 0, WHITE);
+  DrawTextEx(*fonts::skirmish, txt_copyright.c_str(), cpr_position, 
+             cpr_size, -3, WHITE);
 }
