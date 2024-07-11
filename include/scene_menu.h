ed3def83645a579372a4197d8b8bd5c71cd68646
Tyler <lorenarcdev@gmail.com>
2024-07-11 18:13:02 -0400

    Defined destructor.

--- include/scene_menu.h
+++ include/scene_menu.h
@@ -14,6 +14,7 @@
 class MenuScene : public Scene {
 public:
   MenuScene(std::function<void(int)> load_scene);
+  ~MenuScene();
 
   void drawMenuOptions();
   void drawOptionDescription();
--- src/scenes/scene_menu.cpp
+++ src/scenes/scene_menu.cpp
@@ -27,6 +27,14 @@ MenuScene::MenuScene(function<void(int)> load_scene) : Scene(load_scene) {
   };
 
   selected_option = options.begin();
+  PLOGV << "Loaded MainMenu scene.";
+}
+
+MenuScene::~MenuScene() {
+  PLOGV << "Unloading Main Menu scene.";
+  options.clear();
+  options_text.clear();
+  PLOGV << "Main Menu scene unloaded.";
 }
 
 void MenuScene::checkInput() {
