9c0ac1283367683a111d4cae678add638b0b5c19
Tyler <lorenarcdev@gmail.com>
2024-07-11 14:02:29 -0400

    Defined a new type for a vector consisting on 8 bit integers.

--- include/base/generics.h
+++ include/base/generics.h
@@ -1,11 +1,13 @@
 // generics.h
-/* This file is meant to contain foward declarations for specific base
- * classes and type definitions involving said base classes.*/
+/* This file is meant to contain forward declarations for specific base
+ * classes and type definitions.*/
 #pragma once
 #include <vector>
 #include <memory>
+#include <cstdint>
 
 class Combatant;
 class ActionCommand;
 
 typedef std::vector<std::shared_ptr<Combatant>> combatant_list;
+typedef std::vector<uint8_t> menu_options;
