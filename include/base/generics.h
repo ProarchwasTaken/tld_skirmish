a24dacdc94675171419f9b5c218091d233322092
Tyler <lorenarcdev@gmail.com>
2024-07-11 14:39:24 -0400

    Defined a new type for a vector consisting off strings.

--- include/base/generics.h
+++ include/base/generics.h
@@ -4,10 +4,14 @@
 #pragma once
 #include <vector>
 #include <memory>
+#include <string>
 #include <cstdint>
 
 class Combatant;
 class ActionCommand;
 
 typedef std::vector<std::shared_ptr<Combatant>> combatant_list;
+
 typedef std::vector<uint8_t> menu_options;
+typedef std::vector<std::string> menu_options_txt;
+
