6287d7431cb229b9c6f73261d3e7cf5ad9cf1530
Tyler <lorenarcdev@gmail.com>
2024-07-26 14:17:00 -0400

    Finished KnifeLight, having it deal actual damage.

--- include/cmd_knife_light.h
+++ include/cmd_knife_light.h
@@ -1,5 +1,6 @@
 // cmd_knife_light.h
 #pragma once
+#include <raylib.h>
 #include <cstdint>
 #include "base/action_command.h"
 #include "base/generics.h"
@@ -8,9 +9,17 @@
 class KnifeLight : public ActionCommand {
 public:
   KnifeLight(PlayerCharacter *player);
+
+  void setupHurtbox();
   void chargeSequence(float time_elapsed, double &delta_time) override;
   void actSequence(float time_elapsed, double &delta_time) override;
+
+  void enemyHitCheck();
+  void drawDebug() override;
+
+  bool attack_connected = false;
 private:
+  Rectangle hurtbox;
   combatant_list *enemies;
 
   uint16_t damage;
