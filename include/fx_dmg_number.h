66b5232001a5342fb6809046887813f91586a205
Tyler <lorenarcdev@gmail.com>
2024-08-13 13:00:25 -0400

    Defined DamageNumber class. Preparing for testing...

---
+++ include/fx_dmg_number.h
@@ -0,0 +1,21 @@
+// fx_dmg_number.h
+#pragma once
+#include <raylib.h>
+#include "base/dynamic_actor.h"
+
+
+class DamageNumber : public DynamicActor {
+public:
+  DamageNumber(int value, Vector2 position, Color color);
+  ~DamageNumber();
+
+  void update() override;
+  void movement();
+
+  void draw(Vector2 &camera_target) override;
+private:
+  Texture number_texture;
+
+  float movement_speed;
+  float distance_traveled = 0;
+};
