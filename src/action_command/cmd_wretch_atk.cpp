7d1b68cdb9c24c07180459fd48a5e606e1df8f11
Tyler <lorenarcdev@gmail.com>
2024-08-20 10:07:30 -0400

    Provided basic definition for the WretchEnemy's ActionCommand

---
+++ src/action_command/cmd_wretch_atk.cpp
@@ -0,0 +1,57 @@
+// action_command/cmd_wretch_atk.cpp
+#include <raylib.h>
+#include "globals.h"
+#include "base/combatant.h"
+#include "base/action_command.h"
+#include "enemy_wretch.h"
+#include "cmd_wretch_atk.h"
+
+
+WretchAttack::WretchAttack(WretchEnemy *user): 
+  ActionCommand(user, "Wretch Attack", CMD_NONE, 0.2, 0.1, 0.6)
+{
+  damage = 12;
+  guard_pierce = 0.5;
+
+  stun_time = 1.0;
+
+  this->player = user->player;
+  user->current_sprite = sprites::wretch[9];
+  setupHurtbox();
+}
+
+void WretchAttack::setupHurtbox() {
+  float width = 64;
+  float half_width = width / 2;
+
+  float height = 16;
+
+  float x_offset = half_width * user->direction;
+
+  float x = (user->position.x - half_width) + x_offset;
+  float y = user->position.y - 56;
+
+  hurtbox = {x, y, width, height};
+}
+
+void WretchAttack::chargeSequence(float time_elapsed) {
+  ActionCommand::chargeSequence(time_elapsed);
+
+  if (finished_charge) {
+    user->current_sprite = sprites::wretch[10];
+  }
+}
+
+void WretchAttack::actSequence(float time_elapsed) {
+  ActionCommand::actSequence(time_elapsed);
+
+  if (finished_action || attack_connected) {
+    user->current_sprite = sprites::wretch[11];
+  }
+}
+
+void WretchAttack::drawDebug() {
+  if (user->state == ACT) {
+    DrawRectangleLinesEx(hurtbox, 1, MAROON);
+  }
+}
