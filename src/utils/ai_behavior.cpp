4d8f87debac92ffeb991e095906a3a4d39d1f783
Tyler <lorenarcdev@gmail.com>
2024-09-08 12:56:38 -0400

    So apparently these this coding error that I forgot to fix for some reason. Hopefully the tick time is now consistant on all FPS ranges.

--- src/utils/ai_behavior.cpp
+++ src/utils/ai_behavior.cpp
@@ -13,6 +13,6 @@ void AIBehavior::tickPatience(uint8_t &patience, float tick_timestamp,
   bool should_tick = patience != 0 && time_elapsed >= tick_rate;
   if (should_tick) {
     patience--;
-    time_elapsed = CURRENT_TIME;
+    tick_timestamp = CURRENT_TIME;
   }
 }
