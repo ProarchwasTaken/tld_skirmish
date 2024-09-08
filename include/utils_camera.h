c0d9c966351e775153179c0d76c14463a8e5df4f
Tyler <lorenarcdev@gmail.com>
2024-09-08 12:34:26 -0400

    Experimented with const expressions in CameraUtils

--- include/utils_camera.h
+++ include/utils_camera.h
@@ -1,18 +1,24 @@
 // utils_camera.h
 #pragma once
 #include <raylib.h>
+#include "defaults.h"
 #include "base/actor.h"
 
 
 namespace CameraUtils {
-  Camera2D setupCamera();
+  constexpr Vector2 offset = {CANVAS_WIDTH / 2.0, CANVAS_HEIGHT / 2.0};
+  constexpr Vector2 target = {0, 120};
+
+  constexpr Camera2D setupCamera() {
+    return {offset, target, 0, 1};
+  };
 
   /* For having the camera follow a specific x position if its outside
    * a certain threshold.*/
-  void follow(Camera2D &camera, float x_position);
+  void follow(Camera2D &camera, const float x_position);
 
   /* Returns true if the specified actor's tex rect on is on screen. This
    * function is mainly used to check if actor should be drawn.*/
-  bool onScreen(Actor *actor, Vector2 &camera_target);
+  bool onScreen(const Actor *actor, const Vector2 &target);
 }
 
