ca7209b463046494f68955d13a7be6ebcc169673
Tyler <lorenarcdev@gmail.com>
2024-11-03 18:56:40 -0500

    Defined new dynamic actor that will be created upon successful use of GunLight.

---
+++ include/fx_gunprobe.h
@@ -0,0 +1,25 @@
+// fx_gunprobe.h
+#pragma once
+#include "base/combatant.h"
+#include "base/dynamic_actor.h"
+#include "char_player.h"
+
+
+class GunProbe : public DynamicActor {
+public:
+  GunProbe(PlayerCharacter *player, Combatant *probed_enemy);
+
+  void update() override;
+
+  bool shouldDelete();
+  void positionCorrection();
+
+  void draw(Vector2 &camera_target) override;
+  
+private:
+  int8_t direction = RIGHT;
+  float y_level = 102;
+
+  PlayerCharacter *player;
+  Combatant *probed_enemy;
+};
