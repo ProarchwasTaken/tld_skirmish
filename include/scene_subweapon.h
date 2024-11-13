d8e2f2bfafa4fe0f00a1be2ea34ba99e70b2374c
Tyler <lorenarcdev@gmail.com>
2024-11-13 11:38:23 -0500

    Added the Stun Gun to the weapon selection screen.

--- include/scene_subweapon.h
+++ include/scene_subweapon.h
@@ -9,6 +9,7 @@
 
 #define OPT_KNIFE 0
 #define OPT_BALL 1
+#define OPT_GUN 2
 
 #define OPT_YES 0
 #define OPT_NO 1
@@ -40,15 +41,15 @@ public:
   void drawScene() override;
 
 private:
-  std::array<int, 2> icon_ids = {4, 5};
+  std::array<int, 3> icon_ids = {4, 5, 6};
 
   float start_timestamp = 0;
 
-  menu_options weapon_options = {OPT_KNIFE, OPT_BALL};
-  menu_options_txt weapon_name = {"Birthright", "Cornerman"};
+  menu_options weapon_options = {OPT_KNIFE, OPT_BALL, OPT_GUN};
+  menu_options_txt weapon_name = {"Birthright", "Cornerman", "Franklin"};
   menu_options::iterator selected_weapon = weapon_options.begin();
 
-  std::array<SubWeaponData, 2> weapon_data {
+  std::array<SubWeaponData, 3> weapon_data {
     SubWeaponData(
       {"<Cleave>", "Perform a quick\nslash attack.", 3},
       {"<Heave>", "Throw the knife.\nCooldown on use.", 5}
@@ -56,6 +57,10 @@ private:
     SubWeaponData(
       {"<Backcourt>", "Perform a juking\nmaneuver.", 5},
       {"<Foul Play>", "Kick the ball.\nCan be repeated.", 5}
+    ),
+    SubWeaponData(
+      {"<Gee Clip>", "Can deal continuous\n damage.", 2},
+      {"<Conduit>", "Shoots a bolt of\n energy.", 4}
     )
   };
 
