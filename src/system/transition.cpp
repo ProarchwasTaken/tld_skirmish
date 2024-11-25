45740d7dda01e64c18777f047b2be2c7d3c83a50
Tyler <lorenarcdev@gmail.com>
2024-11-25 18:07:19 -0500

    Defined and written WipeTransition system class.

---
+++ src/system/transition.cpp
@@ -0,0 +1,66 @@
+// system/transition.cpp
+#include <cassert>
+#include <raylib.h>
+#include <raymath.h>
+#include "globals.h"
+#include "defaults.h"
+#include "sys_transition.h"
+
+void WipeTransition::interpolate() {
+  if (active == false) {
+    return;
+  }
+
+  if (fading_out == false) {
+    fade_percentage -= GetFrameTime() / fade_time;
+  }
+  else {
+    fade_percentage += GetFrameTime() / fade_time;
+  }
+
+  fade_percentage = Clamp(fade_percentage, 0.0, 1.0);
+  fade_width = Lerp(0.0, CANVAS_WIDTH / 2, fade_percentage);
+  float time_elapsed = CURRENT_TIME - fade_timestamp;
+
+  if (time_elapsed <= fade_time) {
+    active = false;
+  }
+}
+
+void WipeTransition::fadein(float fade_time, Color effect_color) {
+  this->fade_width = CANVAS_WIDTH / 2;
+  this->fade_percentage = 1.0;
+
+  fading_out = false;
+  active = true;
+
+  updateProperties(fade_time, effect_color);
+}
+
+void WipeTransition::fadeout(float fade_time, Color effect_color) {
+  this->fade_width = 0;
+  this->fade_percentage = 0.0;
+
+  fading_out = true;
+  active = true;
+
+  updateProperties(fade_time, effect_color);
+}
+
+void WipeTransition::updateProperties(float fade_time, Color color) {
+  this->effect_color = color;
+  this->fade_time = fade_time;
+  this->fade_timestamp = CURRENT_TIME;
+}
+
+void WipeTransition::draw() {
+  if (fade_width == 0) {
+    return;
+  }
+
+  Rectangle bar = {0, 0, fade_width, CANVAS_HEIGHT};
+  DrawRectangleRec(bar, effect_color);
+
+  bar.x = CANVAS_WIDTH;
+  DrawRectanglePro(bar, {-fade_width, 0}, 0, effect_color);
+}
