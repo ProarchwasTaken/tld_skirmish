c948cbf3138e556298c8989aa279411c11b506a2
Tyler <lorenarcdev@gmail.com>
2024-12-01 13:35:26 -0500

    Implemented the win scene that returns the player to the TitleScene after a couple seconds.

---
+++ include/scene_win.h
@@ -0,0 +1,27 @@
+// scene_win.h
+#pragma once
+#include <string>
+#include <raylib.h>
+#include "base/scene.h"
+#include "game.h"
+
+
+class WinScene : public Scene {
+public:
+  WinScene(Game &skimish);
+
+  void updateScene() override;
+  void interpolateAlpha();
+
+  void drawScene() override;
+private:
+  std::string text = "CLEAR!";
+  Vector2 position = {213, 104};
+  Color txt_color;
+
+  float fade_time = 1.0;
+  float percentage = 1.0;
+
+  float scene_time = 4.0;
+  float scene_timestamp;
+};
