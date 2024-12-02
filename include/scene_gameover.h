af5377cb3a8b78060c7b229725d0690a0403c4a8
Tyler <lorenarcdev@gmail.com>
2024-12-02 12:47:10 -0500

    Fixed the text effect, and now has the image from the previous scene drawn on-screen.

--- include/scene_gameover.h
+++ include/scene_gameover.h
@@ -5,11 +5,17 @@
 #include "base/scene.h"
 #include "game.h"
 
+#define OPT_RESTART 0
+#define OPT_TITLE 1
+#define OPT_QUIT 2
+
 
 class GameOverScene : public Scene {
 public:
-  GameOverScene(Game &skirmish, Texture background);
+  GameOverScene(Game &skirmish, Image screenshot);
   ~GameOverScene();
+
+  void setupBackground(Image &screenshot);
 
   void updateScene() override;
   void interpolateSpacing();
@@ -19,13 +25,14 @@ private:
   std::string text = "GAME OVER";
   Vector2 txt_position = {213, 32};
 
-  float start_spacing = 100;
-  float spacing = start_spacing;
+  float start_spacing = 426;
+  int spacing = start_spacing;
 
   float percentage = 1.0;
-
-  bool ready = false;
   float lerp_time = 0.5;
+  bool ready = false;
 
   Texture background;
+  Rectangle bg_source;
+  Rectangle bg_dest;
 };
--- src/scenes/scene_gameover.cpp
+++ src/scenes/scene_gameover.cpp
@@ -2,22 +2,36 @@
 #include <raylib.h>
 #include <raymath.h>
 #include "base/scene.h"
+#include "defaults.h"
 #include "globals.h"
 #include "game.h"
 #include "utils_text.h"
 #include "scene_gameover.h"
+#include <plog/Log.h>
 
 
-GameOverScene::GameOverScene(Game &skirmish, Texture background) : 
+GameOverScene::GameOverScene(Game &skirmish, Image screenshot): 
   Scene(skirmish) 
 {
   txt_position = Text::alignCenter(fonts::skirmish, text, txt_position, 
                                    2, start_spacing);
-  this->background = background;
+
+  setupBackground(screenshot);
+  UnloadImage(screenshot);
+  PLOGI << "Loaded Game Over scene.";
 }
 
 GameOverScene::~GameOverScene() {
   UnloadTexture(background);
+}
+
+void GameOverScene::setupBackground(Image &screenshot) {
+  background = LoadTextureFromImage(screenshot);
+  float width = GetScreenWidth();
+  float height = GetScreenHeight();
+
+  bg_source = {0, 0, width, height};
+  bg_dest = {0, 0, CANVAS_WIDTH, CANVAS_HEIGHT};
 }
 
 void GameOverScene::updateScene() {
@@ -31,16 +45,19 @@ void GameOverScene::interpolateSpacing() {
   percentage = Clamp(percentage, 0, 1.0);
 
   spacing = Lerp(0, start_spacing, percentage);
-  txt_position = Text::alignCenter(fonts::skirmish, text, txt_position, 
-                                   2, spacing);
+  txt_position = Text::alignCenter(fonts::skirmish, text, {213, 32}, 2, 
+                                   spacing);
 
   if (percentage == 0.0) {
+    PLOGI << "Text interpolation complete.";
     ready = true;
   }
 }
 
 void GameOverScene::drawScene() {
-  int gm_size = fonts::skirmish->baseSize;
+  DrawTexturePro(background, bg_source, bg_dest, {0, 0}, 0, WHITE);
+
+  int gm_size = fonts::skirmish->baseSize * 2;
   DrawTextEx(*fonts::skirmish, text.c_str(), txt_position, gm_size, 
-             spacing, WHITE);
+             spacing, COLORS::PALETTE[32]);
 }
