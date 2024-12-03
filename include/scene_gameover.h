103239a831148029590b221a84f2a3647b531c7c
Tyler <lorenarcdev@gmail.com>
2024-12-03 12:35:35 -0500

    Implemented menu options, making both 'TITLE SCREEN' and 'QUIT GAME' functional.

--- include/scene_gameover.h
+++ include/scene_gameover.h
@@ -2,6 +2,7 @@
 #pragma once
 #include <raylib.h>
 #include <string>
+#include "base/generics.h"
 #include "base/scene.h"
 #include "game.h"
 
@@ -18,21 +19,57 @@ public:
   void setupBackground(Image &screenshot);
 
   void updateScene() override;
+
+  void exitSequence();
+  void endExitSequence();
+
   void interpolateSpacing();
+  void interpolateZoom();
+  void interpolateAlpha();
+
+  void checkInput() override;
+  void selectOption();
+  void drawMenuOptions();
 
   void drawScene() override;
 private:
+  bool ready = false;
+  bool exiting = false;
+
   std::string text = "GAME OVER";
   Vector2 txt_position = {213, 32};
 
   float start_spacing = 426;
   int spacing = start_spacing;
 
-  float percentage = 1.0;
-  float lerp_time = 0.5;
-  bool ready = false;
+  menu_options options {
+    OPT_RESTART,
+    OPT_TITLE,
+    OPT_QUIT
+  };
+
+  menu_options_txt options_text {
+    "RESTART",
+    "TITLE SCREEN",
+    "QUIT GAME",
+  };
+
+  menu_options::iterator selected_option = options.begin();
+
+  float spacing_percentage = 1.0;
+  float spacing_time = 0.75;
+
+  float zoom_percentage = 1.0; 
+  float zoom_time = 4.0;
+
+  float alpha_percentage = 1.0;
+  float alpha_time = 2.0;
+
+  float exit_time = 6.0;
+  float exit_timestamp = 0.0;
 
   Texture background;
+  Color bg_tint = WHITE;
   Rectangle bg_source;
   Rectangle bg_dest;
 };
