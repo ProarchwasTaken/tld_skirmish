5bdd7288ac44cd4c0c0a2db71904e8d012efaf0c
Tyler <lorenarcdev@gmail.com>
2024-12-05 18:41:11 -0500

    Finished the splash screen for now. I did my best with what skills I had at the moment.

--- include/scene_splash.h
+++ include/scene_splash.h
@@ -3,6 +3,7 @@
 #include <string>
 #include "base/scene.h"
 #include "game.h"
+#include "sys_transition.h"
 
 
 class SplashScene : public Scene {
@@ -11,22 +12,35 @@ public:
   ~SplashScene();
 
   void updateScene() override;
-  void interpolationAlpha();
+  void splashSequence(float time_elapsed);
+  void interpolateAlpha();
+  void interpolateSize();
 
   void checkInput() override;
   
   void drawScene() override;
 private:
   Texture icon;
-  Color tint = {255, 255, 255, 0};
+  Vector2 icon_position = {117, 24};
+  Color icon_tint = {255, 255, 255, 0};
 
   std::string text = "\"Our Birthright as players in this game of life"
-  " could never be fully extinquished\".";
-  Vector2 txt_position = {213, 120};
+  " could never be fully extinquished.\"";
+  bool draw_text = true;
+  Vector2 txt_position = {213, 114};
 
   float alpha_percentage = 0.0;
-  float fade_time = 1.0;
+  float icon_fade_time = 1.0;
+
+  float line_size = 0; 
+  float max_line_size = 16;
+
+  float size_percentage = 0.0;
+  float size_time = 1.0;
+
+  WipeTransition bg_transition;
+  bool bg_fading_out = false;
 
   float start_timestamp = 0;
-  float start_time = 8.0;
+  float start_time = 10.0;
 };
--- src/scenes/scene_splash.cpp
+++ src/scenes/scene_splash.cpp
@@ -1,11 +1,13 @@
 // scenes/scene_splash.cpp
 #include <raylib.h>
+#include <raymath.h>
 #include "globals.h"
 #include "base/scene.h"
 #include "utils_text.h"
 #include "game.h"
 #include "scene_title.h"
 #include "scene_splash.h"
+#include <plog/Log.h>
 
 
 SplashScene::SplashScene(Game &skirmish) : Scene(skirmish) {
@@ -15,6 +17,7 @@ SplashScene::SplashScene(Game &skirmish) : Scene(skirmish) {
                                    1, -3);
 
   start_timestamp = CURRENT_TIME;
+  PLOGI << "Loaded Splash Screen.";
 }
 
 SplashScene::~SplashScene() {
@@ -23,9 +26,55 @@ SplashScene::~SplashScene() {
 
 void SplashScene::updateScene() {
   float time_elapsed = CURRENT_TIME - start_timestamp;
+
   if (time_elapsed >= start_time) {
     skirmish->loadScene<TitleScene>();
   }
+  else {
+    splashSequence(time_elapsed);
+    bg_transition.interpolate();
+  }
+}
+
+void SplashScene::splashSequence(float time_elapsed) {
+  if (time_elapsed >= 1.0) {
+    interpolateAlpha();
+  } 
+
+  if (time_elapsed >= 3.0) {
+    interpolateSize();
+  }
+
+  if (bg_fading_out == false && time_elapsed >= 6.0) {
+    bg_transition.fadeout(2, BLACK);
+    bg_fading_out = true;
+  }
+  
+  if (draw_text && time_elapsed >= 9.5) {
+    draw_text = false;
+  }
+}
+
+void SplashScene::interpolateAlpha() {
+  if (alpha_percentage == 1.0) {
+    return;
+  }
+
+  alpha_percentage += GetFrameTime() * start_time;
+  alpha_percentage = Clamp(alpha_percentage, 0.0, 1.0);
+
+  icon_tint.a = Lerp(0, 255, alpha_percentage);
+}
+
+void SplashScene::interpolateSize() {
+  if (size_percentage == 1.0) {
+    return;
+  }
+
+  size_percentage += GetFrameTime() * size_time;
+  size_percentage = Clamp(size_percentage, 0.0, 1.0);
+
+  line_size = Lerp(0, max_line_size, size_percentage);
 }
 
 void SplashScene::checkInput() {
@@ -35,5 +84,19 @@ void SplashScene::checkInput() {
 }
 
 void SplashScene::drawScene() {
+  if (alpha_percentage != 0.0) {
+    DrawTextureV(icon, icon_position, icon_tint);
+  }
 
+  bg_transition.draw();
+
+  if (size_percentage != 0.0) {
+    DrawLineEx({0, 120}, {426, 120}, line_size, BLACK);
+  }
+
+  if (draw_text && line_size == max_line_size) {
+    int size = fonts::skirmish->baseSize;
+    DrawTextEx(*fonts::skirmish, text.c_str(), txt_position, size, -3, 
+               COLORS::PALETTE[42]);
+  }
 }
