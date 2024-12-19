dee909c6d44f7433b1967287556a56c92651ba14
Tyler <lorenarcdev@gmail.com>
2024-12-19 12:06:39 -0500

    Like the control scene, I implemented the smooth transition lerp.

--- include/scene_settings.h
+++ include/scene_settings.h
@@ -9,9 +9,19 @@ public:
   SettingsScene(Game &skirmish);
 
   void updateScene() override;
+  void transitionLerp();
 
   void checkInput() override;
   void drawScene() override;
 private:
   MenuHud menu_hud = MenuHud(false);
+
+  bool ready = false;
+  bool exiting = false;
+
+  float ready_time = 0.25;
+  float ready_percentage = 0.0;
+
+  float box_y = 65;
+  Color box_tint = {255, 255, 255, 0};
 };
--- src/scenes/scene_settings.cpp
+++ src/scenes/scene_settings.cpp
@@ -1,5 +1,6 @@
 // scene/scene_settings.cpp
 #include <raylib.h>
+#include <raymath.h>
 #include "base/scene.h"
 #include "globals.h"
 #include "game.h"
@@ -8,6 +9,9 @@
 #include "scene_settings.h"
 #include <plog/Log.h>
 
+constexpr float BOX_START_Y = 65;
+constexpr float BOX_END_Y = 57;
+
 
 SettingsScene::SettingsScene(Game &skirmish) : Scene(skirmish) {
   PLOGI << "Loaded Settings scene.";
@@ -15,9 +19,38 @@ SettingsScene::SettingsScene(Game &skirmish) : Scene(skirmish) {
 
 void SettingsScene::updateScene() {
   menu_hud.update();
+
+  if (ready == false) {
+    transitionLerp();
+  }
+  else if (exiting) {
+    skirmish->loadScene<MenuScene>(menu_hud);
+  }
+}
+
+void SettingsScene::transitionLerp() {
+  if (exiting == false) {
+    ready_percentage += GetFrameTime() / ready_time;
+  }
+  else {
+    ready_percentage -= GetFrameTime() / ready_time; 
+  }
+
+  ready_percentage = Clamp(ready_percentage, 0.0, 1.0);
+
+  box_y = Lerp(BOX_END_Y, BOX_START_Y, 1.0 - ready_percentage);
+  box_tint.a = Lerp(0, 255, ready_percentage);
+
+  if (ready_percentage == 0.0 || ready_percentage == 1.0) {
+    ready = true;
+  }
 }
 
 void SettingsScene::checkInput() {
+  if (ready == false) {
+    return;
+  }
+
   bool key_x = IsKeyReleased(KEY_X);
 
   bool gamepad_detected = IsGamepadAvailable(0);
@@ -29,7 +62,8 @@ void SettingsScene::checkInput() {
 
   if (key_x || btn_b) {
     SoundUtils::play("opt_cancel");
-    skirmish->loadScene<MenuScene>(menu_hud);
+    ready = false;
+    exiting = true;
   }
 }
 
@@ -38,5 +72,5 @@ void SettingsScene::drawScene() {
   menu_hud.draw();
   DrawTexture(*sprites::hud_mainmenu[7], 4, 21, WHITE);
 
-  DrawTexture(*sprites::hud_menubox[1], 109, 57, WHITE);
+  DrawTexture(*sprites::hud_menubox[1], 109, box_y, box_tint);
 }
