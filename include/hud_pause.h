1cba1a41e0c849d6c0a4c0213b137315fa1c4765
Tyler <lorenarcdev@gmail.com>
2024-12-25 12:38:13 -0500

    Implemented menu options, and integrated MenuButtonsHud into the pause menu itself.

--- include/hud_pause.h
+++ include/hud_pause.h
@@ -2,6 +2,11 @@
 #pragma once
 #include "base/generics.h"
 #include "hud_menu.h"
+#include "hud_menu_btn.h"
+
+#define OPT_RESUME 0
+#define OPT_TITLE 1
+#define OPT_QUIT 2
 
 
 class PauseHud : public MenuHud {
@@ -15,4 +20,20 @@ public:
   void draw() override;
 private:
   GameplayScene *scene;
+
+  menu_options options = {
+    OPT_RESUME,
+    OPT_TITLE,
+    OPT_QUIT
+  };
+
+  menu_options_txt options_txt = {
+    "RESUME",
+    "TITLE",
+    "QUIT"
+  };
+
+  menu_options::iterator selected_option = options.begin();
+  MenuButtonsHud menu_btns = MenuButtonsHud(options, options_txt,
+                                            selected_option, this);
 };
--- src/hud/hud_pause.cpp
+++ src/hud/hud_pause.cpp
@@ -1,6 +1,8 @@
 // hud/hud_pause.cpp
 #include <raylib.h>
 #include "scene_gameplay.h"
+#include "utils_menu.h"
+#include "utils_sound.h"
 #include "hud_menu.h"
 #include "hud_pause.h"
 
@@ -16,14 +18,44 @@ void PauseHud::restartOpening() {
 }
 
 void PauseHud::checkInput() {
+  if (opening && spade_state != SPADE_STANDBY) {
+    return;
+  }
 
+  bool key_down = IsKeyPressed(KEY_DOWN);
+  bool key_up = IsKeyPressed(KEY_UP);
+  bool key_z = IsKeyPressed(KEY_Z);
+
+  bool gamepad_detected = IsGamepadAvailable(0);
+  bool btn_down = false; 
+  bool btn_up = false;
+  bool btn_a = false;
+
+  if (gamepad_detected) {
+    btn_down = IsGamepadButtonPressed(0, GAMEPAD_BUTTON_LEFT_FACE_DOWN);
+    btn_up = IsGamepadButtonPressed(0, GAMEPAD_BUTTON_LEFT_FACE_UP);
+    btn_a = IsGamepadButtonPressed(0, GAMEPAD_BUTTON_RIGHT_FACE_DOWN);
+  }
+  
+  if (key_down || btn_down) {
+    Menu::nextOption(options, selected_option, false);
+  }
+  else if (key_up || btn_up) {
+    Menu::previousOption(options, selected_option, false);
+  }
 }
 
 void PauseHud::update() {
   MenuHud::update();
+  menu_btns.update();
 }
 
 void PauseHud::draw() {
   MenuHud::draw();
+
+  if (opening == false) {
+    DrawTexture(*sprites::hud_mainmenu[8], 4, 21, WHITE);
+    menu_btns.drawMenuButtons();
+  }
 }
 
